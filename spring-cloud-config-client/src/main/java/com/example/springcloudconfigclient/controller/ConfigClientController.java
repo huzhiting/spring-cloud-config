fe7c7b6113151ad3c25f9a1b1a1fc059303afc48
huzhiting <zhitinghu@mjitech.com>
2019-04-24 16:40:48 +0800

    update config

--- spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/controller/ConfigClientController.java
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/controller/ConfigClientController.java
@@ -1,4 +1,17 @@
 package com.example.springcloudconfigclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConfigClientController {
+
+    @Value("${config.env}") // git配置文件里的key
+    String configEnv;
+
+    @RequestMapping("/getConfigInfo")
+    public String getConfigInfo(){
+        return "获取环境配置：" +  configEnv;
+    }
 }
