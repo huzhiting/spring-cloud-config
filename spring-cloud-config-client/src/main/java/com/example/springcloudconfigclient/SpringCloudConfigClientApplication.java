c27b76b12db1cd629daed6d24b5dce976fab6973
huzhiting <zhitinghu@mjitech.com>
2019-04-25 11:18:15 +0800

    'comments'

--- spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -2,8 +2,6 @@ package com.example.springcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 public class SpringCloudConfigClientApplication {
@@ -11,12 +9,5 @@ public class SpringCloudConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigClientApplication.class, args);
     }
-
-//    @Bean
-//    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-//        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
-//        c.setIgnoreUnresolvablePlaceholders(true);
-//        return c;
-//    }
 
 }
