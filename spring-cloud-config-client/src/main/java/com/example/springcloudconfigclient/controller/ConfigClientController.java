52f2b1aba9b654aedbae21d702d68edff2e397b9
huzhiting <zhitinghu@mjitech.com>
2019-04-25 16:56:01 +0800

    'autoupdate'

--- spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/controller/ConfigClientController.java
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.example.springcloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${env}") // git配置文件里的key
